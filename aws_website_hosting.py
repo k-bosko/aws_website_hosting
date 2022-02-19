4ccf19f746b302db84f0c173bab0ed7d15d301ed
Katerina Bosko <katerinabosko@gmail.com>
2022-02-18 21:37:02 -0800

    final version of website migration script to aws

--- aws_website_hosting.py
+++ aws_website_hosting.py
@@ -3,51 +3,108 @@ import os
 import requests
 import zipfile
 import io
+import json
 
-def upload_directory(path, bucketname, s3):
+
+def upload_directory(path, bucketname, client):
+    print("starting upload")
     for root, dirs, files in os.walk(path):
         for file in files:
-            s3.meta.client.upload_file(os.path.join(root,file),bucketname,file)
+            relative_path = os.path.join(root, file)
+            path_to_upload = relative_path.replace('aws_website_hosting-main/', '')
+            print("uploading: ", path_to_upload)
+            extension = file.split('.')[-1]
+            mode = 'rb' if extension in {'png', 'ico', 'jpg', 'pdf'} else 'r'
+
+            if extension == 'html':
+                content_type = 'text/html'
+            elif extension == 'css':
+                content_type = 'text/css'
+            elif extension == 'js':
+                content_type = 'application/javascript'
+            elif extension == 'pdf':
+                content_type = 'application/pdf'
+            elif extension == 'jpg':
+                content_type = 'image/jpeg'
+            elif mode == 'r':
+                content_type = 'text/plain'
+            else:
+                content_type = 'binary/octet-stream'
+
+            with open(relative_path, mode=mode) as fp:
+                params = {
+                    'Bucket': bucketname,
+                    'Body': fp.read(),
+                    'Key': path_to_upload,
+                    'ContentType': content_type,
+                }
+                client.put_object(**params)
+
 
 def main():
-    site_path = 'aws_website_hosting'
-    if not os.path.exists(site_path):
-        os.mkdir(site_path)
+    if not os.path.isdir('aws_website_hosting-main'):
+        # get the data from link
+        url = "https://github.com/k-bosko/aws_website_hosting/archive/refs/heads/main.zip"
+        r = requests.get(url, stream=True)
+        # unzip it
+        with zipfile.ZipFile(io.BytesIO(r.content), 'r') as zip_ref:
+            zip_ref.extractall()
 
-    # get the data from link
-    url = "https://github.com/k-bosko/aws_website_hosting/archive/refs/heads/main.zip"
-    r = requests.get(url, stream=True)
-    # unzip it
-    with zipfile.ZipFile(io.BytesIO(r.content), 'r') as zip_ref:
-        zip_ref.extractall(site_path)
-
-    # access ressource and name it s3
+    # create s3 resource
     s3 = boto3.resource('s3')
-    bucket_name = 'lab1-kbosko'
+    # create a client
+    client = boto3.client('s3')
+
+    bucket_name = 'lab1kbosko'
+    site_path = 'aws_website_hosting-main'
 
     #list buckets
     buckets = s3.buckets.all()
 
     #delete if bucket with this name exists
-    if s3.Bucket(bucket_name) in buckets:
-        s3.delete_bucket(Bucket='lab1-kbosko', )
+    bucket_to_delete = s3.Bucket(bucket_name)
+    if bucket_to_delete in buckets:
+        print(f"deleting existing bucket `{bucket_name}`")
+        bucket_to_delete.Acl().put(ACL='public-read-write')
+        bucket_to_delete.objects.all().delete()
+        bucket_to_delete.delete()
 
-    #create bucket
-    s3.create_bucket(Bucket=bucket_name)
+    #create bucket with public-read permission
+    s3.create_bucket(ACL='public-read', Bucket=bucket_name)
 
     # print existing bucket names for debugging
     for bkt in buckets:
         print(bkt.name)
 
+    #add website configuration to a bucket
+    client.put_bucket_website(
+        Bucket=bucket_name,
+        WebsiteConfiguration={
+            'IndexDocument': {
+                'Suffix': 'index.html',
+            },
+        },
+    )
+
+    policy = { "Version": "2012-10-17", "Statement": [
+                {
+                    "Sid": "PublicReadGetObject",
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": "s3:GetObject",
+                    "Resource": f"arn:aws:s3:::{bucket_name}/*"
+                }
+            ]
+        }
+
+    #add bucket policy
+    client.put_bucket_policy(
+        Bucket=bucket_name,
+        Policy=json.dumps(policy),
+    )
+
     #copy files to s3
-    upload_directory(site_path, bucket_name, s3)
-
-    #instantiate bucket
-    bucket = s3.Bucket(bucket_name)
-
-    #update permission
-    bucket.Acl().put(ACL='public-read')
-
+    upload_directory(site_path, bucket_name, client)
 
 if __name__ == '__main__':
     main()
